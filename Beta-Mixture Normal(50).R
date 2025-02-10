8a3d953f2f6096656f19ffee68ea81355d58a7c9
unknown <kontemeniotisn@gmail.com>
2025-02-10 14:22:16 +0200

    parallel

--- Beta-Mixture Normal(50).R
+++ Beta-Mixture Normal(50).R
@@ -1,47 +1,53 @@
 library(dcov)
+library(Rfast)
+library(Rfast2)
+library(parallel)
 
-beta <- function(size) {    #Βάζω τυχαίες παραμέτρους για όλες τις κατανομές
-  a <- runif(1, 1, 50)
-  b <- runif(1, 1, 50)
+beta <- function(size) {
+  a <- Rfast2::Runif(1, 1, 50)
+  b <- Rfast2::Runif(1, 1, 50)
   x <- rbeta(size, a, b)
   return(x)
 }
+
 mix_norm <- function(size) {
   y <- c()
-  p <- runif(1) #Επιλέγω τυχαία πιθανότητα p για τις τιμές από τη 1η κανονική κατανομή
-  mu1 <- runif(1, -50, 50)
-  sigma1 <- runif(1, 0, 50)
-  mu2 <- runif(1, -50, 50)
-  sigma2 <- runif(1, 0, 50)
+  p <- Rfast2::Runif(1)
+  mu1 <- Rfast2::Runif(1, -50, 50)
+  sigma1 <- Rfast2::Runif(1, 0, 50)
+  mu2 <- Rfast2::Runif(1, -50, 50)
+  sigma2 <- Rfast2::Runif(1, 0, 50)
   for (i in 1:size) {
-    q <- runif(1)
-    if (q <= p) {
-      y[i] <- rnorm(1, mu1, sigma1)
-    } else{
-      y[i] <- rnorm(1, mu2, sigma2)
-    }
+    y[i] <- p * Rfast::Rnorm(1, mu1, sigma1) + (1 - p) * Rfast::Rnorm(1, mu2, sigma2)
   }
   return(y)
 }
 
+sim_error <- function(dummy, n) {
+  x <- beta(n)
+  y <- mix_norm(n)
+  pperm <- dcor.test(x, y, R = 1000, type = 'U')$p.values
+  stat_as <- n * dcov::dcor(x, y, type = "U")^2 + 1
+  pas <- pchisq(stat_as, 1, lower.tail = FALSE)
+  c(perm = as.numeric(pperm < 0.05),
+    asym = as.numeric(pas < 0.05))
+}
 
-type1_error <- function(P) {
-  count_perm <- 0
-  count_as <- 0
-  for (i in 1:P) {
-    x <- beta(50)
-    y <- mix_norm(50)
-    pperm <- dcor.test(x, y, R = 1000, type = 'U')$p.values
-    stat_as <- 50 * dcor(x, y, type = "U")^2 + 1
-    pas <- pchisq(stat_as, 1, lower.tail = FALSE)
-    if (pperm < 0.05) {
-      count_perm <- count_perm + 1
-    }
-    if (pas < 0.05) {
-      count_as <- count_as + 1
-    }
-  }
-  type1 <- c("Pemutation" = count_perm / P, "Asymptotic" = count_as / P)
+type1_error_parallel <- function(P, n) {
+  cl <- makeCluster(detectCores())
+  clusterEvalQ(cl, {
+    library(dcov)
+    library(Rfast)
+    library(Rfast2)
+  })
+  clusterExport(cl, varlist = c("beta", "mix_norm", "sim_error"), envir = environment())
+  res_list <- parLapply(cl, 1:P, function(i) sim_error(i, n))
+  stopCluster(cl)
+  res <- do.call(rbind, res_list)
+  type1 <- c("Permutation" = mean(res[, "perm"]),
+             "Asymptotic"  = mean(res[, "asym"]))
   return(type1)
 }
-type1_error(1000)
+
+result <- type1_error_parallel(P = 1000, n = 100)
+microbenchmark(type1_error_parallel(P = 1000, n = 100),1,unit='seconds')
