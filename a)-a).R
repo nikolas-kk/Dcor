e20c6ff78f7c7455cfed82e59060131f9a6ac86a
rafasvar <rafailvargiakakis@gmail.com>
2025-02-10 14:35:11 +0200

    akuto

--- a)-a).R
+++ a)-a).R
@@ -54,4 +54,20 @@ df <- data.frame(aa)
 ggplot(df, aes(x = aa)) +
   geom_density()
 
-data.frame(CocktailNorm(10))
+d <-replicate(1000,CocktailNorm(500,runif(1,0,1)))
+y <-replicate(1000,rnorm(500,runif(1,1,10),runif(1,1,10)))
+
+pperm <- dcor.test(d, y, R = 1000, type = 'U')$p.values
+dcor.te
+stat_as <- 50 * dcor(x, y, type = "U")^2 + 1
+pas <- pchisq(stat_as, 1, lower.tail = FALSE)
+if (pperm < 0.05) {
+  count_perm <- count_perm + 1
+}
+if (pas < 0.05) {
+  count_as <- count_as + 1
+}
+}
+type1 <- c("Pemutation" = count_perm / P, "Asymptotic" = count_as / P)
+return(type1)
+}
