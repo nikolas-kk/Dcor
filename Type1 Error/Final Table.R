587077119761e87aaf85fbe812a7d872357ef3c3
unknown <kontemeniotisn@gmail.com>
2025-02-12 22:13:38 +0200

    ..

--- Type1 Error/Final Table.R
+++ Type1 Error/Final Table.R
@@ -1,4 +1,4 @@
 library(kableExtra)
-
-final_table <- as.data.frame(cbind(Beta_Res, Cauchy_Res, Gamma_Res
-                                   , SkewNormal_Res, von_results))
+library(dplyr)
+final_table <- t(as.data.frame(cbind(Beta_Res, Cauchy_Res, Gamma_Res
+                                   , SkewNormal_Res, von_results)))
