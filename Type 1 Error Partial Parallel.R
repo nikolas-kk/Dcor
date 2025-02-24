753a3d16268bec29db996c2f402730e5df704d57
unknown <kontemeniotisn@gmail.com>
2025-02-24 15:08:25 +0200

    diafora

--- Type 1 Error Partial Parallel.R
+++ Type 1 Error Partial Parallel.R
@@ -1,4 +1,5 @@
-source("C:/Users/Nikolas/Desktop/Αρχεία R/Dcor/Type1 Error/Distributions&Type1 Partial.R")
+source('C:/Users/Nikolas/Desktop/Arxeia_R/Dcor/Type1 Error/Distributions&Type1_Partial.R')
+source('C:/Users/Nikolas/Desktop/pdcor2.R')
 library(parallel)
 library(doParallel)
 library(foreach)
@@ -7,7 +8,7 @@ library(Rfast2)
 library(LaplacesDemon)
 library(mixsmsn)
 
-ncores <- detectCores() - 7
+ncores <- detectCores() - 8
 cl <- makeCluster(ncores)
 registerDoParallel(cl)
 
@@ -17,7 +18,7 @@ type1_error_parallel <- function(P, n, distx, disty, nz) {
   dist_y <- match.fun(disty)
   
   results <- foreach(i = 1:P, .combine = rbind, .packages = c("dcov", "Rfast2", "LaplacesDemon", "mixsmsn"), 
-                     .export = c("z1", "z2")) %dopar% {
+                     .export = c("z1", "z2","pdcor.test2",'pdcor2')) %dopar% {
                        if (nz == 1) {
                          z <- z1(n)
                        } else {
@@ -27,8 +28,8 @@ type1_error_parallel <- function(P, n, distx, disty, nz) {
                        x <- dist_x(n)
                        y <- dist_y(n)
                        
-                       pperm   <- dcov::pdcor.test(x, y, z, R = 500, type = 'U')$p.values
-                       stat_as <- n * dcov::pdcor(x, y, z, type = "U") + 1
+                       pperm   <-pdcor.test2(x, y, z, R = 500)$
+                       stat_as <- n * pdcor2(x,y,z) + 1
                        pas     <- pchisq(stat_as, 1, lower.tail = FALSE)
                        gc()
                        
@@ -38,7 +39,7 @@ type1_error_parallel <- function(P, n, distx, disty, nz) {
   colMeans(results)
 }
 
-n_values <- c(50, 100, 200, 500,1000,2000,5000,10000)
+n_values <- c(10000)
 distx <- "beta"
 distributions <- c("mix_norm", "mix_norm3", "mix_skew_t")
 
@@ -47,7 +48,7 @@ results <- list()
 for (disty in distributions) {
   for (n in n_values) {
     key <- paste0("n", n, "_", disty)
-    results[[key]] <- type1_error_parallel(1000, n, distx, disty, nz = 1)
+    results[[key]] <- type1_error_parallel(1000, n, distx, disty, nz = 2)
   }
 }
 
