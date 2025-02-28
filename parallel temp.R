47b2c97138d70c3f9abdecf4451e59ecc91c970d
nikolas-kk <kontemeniotisn@gmail.com>
2025-02-28 21:56:58 +0200

    τελικο

--- parallel temp.R
+++ parallel temp.R
@@ -1,14 +1,13 @@
 source('~/Desktop/Dcor/Pdcor1zfast.R')
 source('~/Desktop/Dcor/Type1 Error/Distributions&Type1_Partial.R')
+library(parallel)
 
 #fix the function and use mclapply for linux
 P <- 1000
-power_test <- function(P,n,distx,disty,nz=1){
-  pperm <-numeric(P)
-  pas <- numeric (P)
+power_test <- function(n,distx,disty,nz=1){
   dist_x <- match.fun(distx)
   dist_y <- match.fun(disty)
-  for (i in 1:P){
+  
   if (nz == 1) {
     z <- z1(n)
   } else {
@@ -17,21 +16,24 @@ power_test <- function(P,n,distx,disty,nz=1){
   y <- numeric(n)
   x <- numeric(n)
   z_bar <- mean(z)
-  id1 <- z <= z_bar
-  id2 <- !id1
-  size1 <- sum(id1)
+  id <- z <= z_bar
+  size1 <- sum(id)
   size2 <- n - size1
-  z_1 <- z[id1]
-  z_2 <- z[id2]^2
-  x[id1] <- dist_x(size1) + z_1
-  x[id2] <- dist_x(size2) + z_2
-  y[id1] <- dist_y(size1) + z_1
-  y[id2] <- dist_y(size2) + z_2
+  z_1 <- z[id]
+  z_2 <- z[!id]^2
+  x[id] <- dist_x(size1) + z_1
+  x[!id] <- dist_x(size2) + z_2
+  y[id] <- dist_y(size1) + z_1
+  y[!id] <- dist_y(size2) + z_2
   
-  pperm[i]   <-pdcor.test3(x, y, z, R = 500)
+  pperm   <-pdcor.test3(x, y, z, R = 500)
   stat_as <- n * pdcor2(x,y,z) + 1
-  pas[i]    <- pchisq(stat_as, 1, lower.tail = FALSE)
-  }
-  c(mean(pperm<0.05),mean(pas<0.05))
+  pas    <- pchisq(stat_as, 1, lower.tail = FALSE)
+  c(Permutation = as.numeric(pperm < 0.05), Asymptotic = as.numeric(pas < 0.05))
 }
-microbenchmark::microbenchmark(power_test(1000,100,'beta','mix_norm'))
+results <- mclapply(1:P, function(i) {
+  power_test(n, distx, disty, nz)
+}, mc.cores = detectCores() - 7)
+results_matrix <-do.call(rbind,results)
+results_matrix
+colMeans(results_matrix)
