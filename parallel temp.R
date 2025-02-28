c328fe45808abe815cb838e1faae557285df4ea1
nikolas-kk <kontemeniotisn@gmail.com>
2025-02-28 23:05:34 +0200

    το κραταμε

--- parallel temp.R
+++ parallel temp.R
@@ -26,14 +26,33 @@ type1error_testpartial <- function(n,distx,disty,nz=1){
   y[id] <- dist_y(size1) + z_1
   y[!id] <- dist_y(size2) + z_2
   
-  pperm   <-pdcor.test3(x, y, z, R = 500)
-  stat_as <- n * pdcor2(x,y,z) + 1
-  pas    <- pchisq(stat_as, 1, lower.tail = FALSE)
-  c(Permutation = as.numeric(pperm < 0.05), Asymptotic = as.numeric(pas < 0.05))
+  pperm   <-pdcor.test3(x,y,z)
+  c(Permutation = as.numeric(pperm < 0.05))
 }
-results <- mclapply(1:P, function(i) {
-  type1error_testpartial(n, distx, disty, nz)
-}, mc.cores = detectCores() - 7)
+microbenchmark::microbenchmark({results <- mclapply(1:1000, function(i) {
+  type1error_testpartial(1000, 'cauchy','mix_skew_t', nz)
+}, mc.cores = detectCores() - 1)},times=10)
 results_matrix <-do.call(rbind,results)
-results_matrix
 colMeans(results_matrix)
+pperm<-numeric(n)
+pas<-numeric(n)
+
+
+p_values <- numeric(1000)
+for (i in 1:1000) {
+  y <- numeric(n)
+  x <- numeric(n)
+  z_bar <- mean(z)
+  id <- z <= z_bar
+  size1 <- sum(id)
+  size2 <- n - size1
+  z_1 <- z[id]
+  z_2 <- z[!id]^2
+  x[id] <- dist_x(size1) + z_1
+  x[!id] <- dist_x(size2) + z_2
+  y[id] <- dist_y(size1) + z_1
+  y[!id] <- dist_y(size2) + z_2
+  p_values[i] <- dcov::pdcor.test(x,y,z,type='U')$p.values
+}
+sum(p_values < 0.05) / 1000
+Rfast::permcor(x,y)$p-value
